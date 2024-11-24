ac6104118ee98557ec1a1c5d9b39648c0474c0bc
Phanthom-Mekat <jahidulhossainmekat@gmail.com>
2024-11-24 20:15:32 +0600

    basic setup init

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/home/Navbar";
+
+const MainLayout = () => {
+    return (
+        <div>
+            {/* navbar here */}
+            <Navbar/>
+           <Outlet/>
+
+        </div>
+    );
+};
+
+export default MainLayout;
