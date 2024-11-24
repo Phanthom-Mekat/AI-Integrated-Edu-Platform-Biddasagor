2a15735733807e9b483c8d3eadbfbb3e1b2256e3
Phanthom-Mekat <jahidulhossainmekat@gmail.com>
2024-11-24 21:21:09 +0600

    basic navbar setup

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -1,8 +1,48 @@
+import { GiBookAura } from "react-icons/gi";
+import { IoMdKey } from "react-icons/io";
+import {Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+    const links = <>
+        <li className="mr-2 font-bold text-gray-500"><NavLink to="/">Home</NavLink></li>
+        <li className="mr-2 font-bold text-gray-500"><NavLink to="/about">About Us</NavLink></li>
+    </>
     return (
-        <div>
-            navbar
+        <div className="w-11/12 mx-auto">
+            <div className="navbar">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            {links}
+                        </ul>
+                    </div>
+                    <Link to="/" className="btn btn-ghost text-xl">Biddasagor<GiBookAura className="text-secondary" /></Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {links}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn btn-ghost btn-outline text-secondary bg-white rounded-3xl"><IoMdKey className="text-lg" />Login</a>
+                </div>
+            </div>
         </div>
     );
 };
